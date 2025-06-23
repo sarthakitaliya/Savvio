c2c106627a7142956d29498849a35442fe559919
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-06-23 11:40:41 +0530

    feat: initialize React extension with Vite and TypeScript setup

---
+++ apps/extension/src/popup.tsx
@@ -0,0 +1,8 @@
+// src/popup.tsx
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <div>Hello from React Extension 👋</div>
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
