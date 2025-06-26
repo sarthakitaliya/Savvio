6c48b9ef81b4a40d7649da1f5504b875d703cf2b
Rishi6jp <rainbro187@gmail.com>
2025-06-26 20:06:16 +0530

    added tailwind

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { signIn } from "../../lib/auth-client";
+import { GoogleIcon } from "../../public/icons/google";
 
 export default function LoginPage() {
   const handleGoogleLogin = async () => {
@@ -13,13 +14,23 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <button
-        onClick={handleGoogleLogin}
-        className="px-6 py-3 bg-blue-600 text-white rounded"
-      >
-        Sign in with Google
-      </button>
+    <div className=" flex flex-col justify-center items-center min-h-screen w-full">
+      <div className="flex flex-grow  flex-col gap-2 items-center ">
+        <div className="text-4xl font-bold">Login to Mark-me</div>
+        <button className="w-full border-[#202020] border-2 flex gap-2 justify-center rounded-sm hover:bg-[#202020] transition-all duration-300 ease-out ease-in items-center h-10" onClick={handleGoogleLogin}>
+            <GoogleIcon/>  
+            <span>Continue with Google</span>
+        </button>
+      </div>
+
+      
+      
+      <div className="flex justify-center">Return home</div>
+      <div className="flex justify-center gap-3 text-[10px]">
+          <a href="">Term of Service</a>
+          <a href="">Privacy Policy</a>
+      </div>
+      
     </div>
   );
 }
