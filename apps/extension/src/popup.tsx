70e9fde3d0e7d65765b571a4939d2a482cade3bb
Bhuvnesh <153991654+Rishi6jp@users.noreply.github.com>
2025-06-26 23:25:46 +0530

    Delete apps directory

--- apps/extension/src/popup.tsx
+++
@@ -1,8 +0,0 @@
-// src/popup.tsx
-import ReactDOM from 'react-dom/client'
-
-function App() {
-  return <div>Hello from React Extension 👋</div>
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
