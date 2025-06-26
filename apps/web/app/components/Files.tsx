6f2a8b75d48feb041f6940a4a0960bfd06599c94
Rishi6jp <rainbro187@gmail.com>
2025-06-26 23:08:16 +0530

    tried adding card

--- apps/web/app/components/Files.tsx
+++ apps/web/app/components/Files.tsx
@@ -1,5 +1,11 @@
-export const Files = () => {
-    return <div className="border-1 h-24 rounded-md w-34 border-white ">
-        helo
+export interface FilesProp {
+    name: String
+}
+
+
+export const Files = (props: FilesProp) => {
+    return <div className="hover:border-1 hover:border-white h-24 rounded-xl w-44 bg-[#191919] overflow-hidden">
+                <div className="h-1/3 bg-[#202020]"></div>
+                <div className="h-2/3 flex items-center text-xl justify-start p-5">{props.name}</div>
     </div>
 }
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -2,19 +2,32 @@
 
 import React from "react";
 import { Files } from "../components/Files";
+import { Cardmark } from "../components/Card";
 
 export default function Dashboard() {
   
 
   return (
 
-    <div className="relative bg-[#171717] h-screen flex-1 flex-col ">
-        <div>
-
+    <div className="relative bg-[#171717] h-screen flex-1 flex-col text-white mx-auto flex justify-center items-center ">
+        <div className="h-1/2 w-screen flex justify-center items-center flex-col gap-4">
+            <div className="text-4xl font-bold">
+                Search You Mark
+            </div>
+            <input type="text" placeholder="Search . . ."className=" text-white w-1/3  rounded-2xl p-2 outline-none bg-[#202020]  placeholder:text-center"/>           
         </div>
-        <div></div>
-        <div>
-
+        <div className="fixed bottom-3 right-3 p-2 px-4 rounded-md bg-[#202020] hover:bg-[#3d3d3d]">+</div>
+        <div className="grid grid-cols-5 gap-6 w-1/2 place-content-center " >
+            <Files name="sports"/>
+            <Files name="sports"/>
+            <Cardmark title="Lofi Music" description="Words for me" type="youtube" link="https://youtu.be/YkuAm0ZLTfY?si=qwn0nQRDv0jRwNYA"/>
+            <Files name="sports"/>
+            <Files name="sports"/>
+            <Files name="sports"/>
+            <Files name="sports"/>
+            <Files name="sports"/>
+            <Files name="sports"/>
+            <Files name="sports"/>
         </div>
     </div>
 
@@ -26,13 +39,10 @@ export default function Dashboard() {
         
     //     <div className="flex flex-col gap-3 bg-[#171717] text-white justify-center items-center w-full h-full overflow-auto">
     //         <div className="h-300">
-    //                 <div className="text-4xl font-bold">
-    //                     Search You Mark
-    //                 </div>
-    //                 <input type="text" placeholder="Search . . ."className=" text-white w-1/3  rounded-2xl p-2 outline-none bg-[#202020]  placeholder:text-center"/>
+    //                 
     //         </div>
             
-    //         <div className="fixed bottom-3 right-3 p-2 px-4 rounded-md bg-[#202020] hover:bg-[#3d3d3d]">+</div>
+    //         
     //         <div className=" grid grid-cols-4 gap-4 mt-10">
     //             <Files/>
     //             <Files/>
