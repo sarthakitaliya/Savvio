7369fd57c5edb1e07e4593331fdd22ff8ffa4217
Rishi6jp <rainbro187@gmail.com>
2025-06-26 23:24:03 +0530

    fixed issues

---
+++ apps/web/app/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { Bookmark } from "../../public/icons/bookmark";
+import { Dark } from "../../public/icons/moon";
+import { PersonIcon } from "../../public/icons/person";
+
+export const NavBar = () => {
+  return (
+    <nav className="w-screen bg-[#202020] text-white flex justify-between items-center h-15 px-10">
+      <div className="flex gap-4">
+        <Bookmark />
+        Mark
+      </div>
+      <div className="flex gap-4">
+        <button><Dark /></button>
+        <button><PersonIcon /></button>
+      </div>
+    </nav>
+  );
+};
+
