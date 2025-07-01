3204645c235553e71ab7fea69363c0ba554ff025
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-07-02 03:52:42 +0530

    feat: add default folder for new user

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,18 +1,47 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prismaClient } from "@repo/db";
- 
+import { createAuthMiddleware } from "better-auth/api";
+
 export const auth = betterAuth({
-    database: prismaAdapter(prismaClient, {
-        provider: "postgresql",
+  database: prismaAdapter(prismaClient, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  emailAndPassword: {
+    enabled: false,
+  },
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      const session = ctx.context.session;
+      console.log("server", session);
+      const now: any = new Date();
+      if (
+        session?.user.createdAt &&
+        now - new Date(session.user.createdAt).getTime() < 60_000
+      ) {
+        console.log("User created within the last minute");
+        const existingFolder = await prismaClient.folder.findFirst({
+          where: {
+            userId: session.user.id,
+            name: "Unsorted",
+          },
+        });
+        if (!existingFolder) {
+          const defaultFolder = await prismaClient.folder.create({
+            data: {
+              name: "Unsorted",
+              userId: session.user.id,
+            },
+          });
+          console.log("Default folder created:", defaultFolder);
+        }
+      }
     }),
-   socialProviders:{
-    google:{
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }
-   },
-   emailAndPassword: {
-    enabled: false
-   }
+  },
 });
