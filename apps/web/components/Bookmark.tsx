6419f737d87ef7305b67c089700e8545c8262dfe
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-07-06 16:22:28 +0530

    refactor: improve conditional rendering for favicon and preview image in Bookmark component

--- apps/web/components/Bookmark.tsx
+++ apps/web/components/Bookmark.tsx
@@ -4,17 +4,18 @@ dayjs.extend(relativeTime);
 import type { Bookmark } from "@repo/types";
 
 export function Bookmark({ bookmark }: { bookmark: Bookmark }) {
-    
   return (
     <div className="w-full sm:w-64 max-w-xs flex-grow bg-white border-[0.1px] border-gray-300 dark:border-white/20 dark:bg-[#2A2A2A] p-4 rounded-2xl">
       <a href={bookmark.url || undefined} target="_blank" className="block">
         <div className="flex items-center gap-2 mb-2">
           <img
-            src={bookmark?.favicon ?? "/default-favicon.png"}
+            src={bookmark?.favicon ? bookmark.favicon : "/default-favicon.png"}
             alt="Bookmark favicon"
             className="w-4 h-4 object-contain"
           />
-          <h3 className="text-md truncate flex-1">{bookmark.title || "Untitled"}</h3>
+          <h3 className="text-md truncate flex-1">
+            {bookmark.title || "Untitled"}
+          </h3>
           <div className="self-end ml-auto">
             <span className="text-xs text-gray-400 dark:text-gray-500">
               {dayjs(bookmark.createdAt).fromNow()}
@@ -22,7 +23,11 @@ export function Bookmark({ bookmark }: { bookmark: Bookmark }) {
           </div>
         </div>
         <img
-          src={bookmark?.previewImage ?? "/default-preview.png"}
+          src={
+            bookmark?.previewImage
+              ? bookmark.previewImage
+              : "/default-preview.png"
+          }
           alt="Bookmark screenshot"
           className="w-full h-32 object-cover rounded-md mt-2"
         />
