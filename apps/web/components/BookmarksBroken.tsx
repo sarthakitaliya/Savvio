6baa8e7b88d4975b9caf400c82500677e6a2b465
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-07-18 00:50:15 +0530

    feat: enhance styling in BookmarksBroken and HowItWorks components; adjust layout in Hero component

--- apps/web/components/BookmarksBroken.tsx
+++ apps/web/components/BookmarksBroken.tsx
@@ -4,7 +4,7 @@ export default function BookmarksBroken() {
   return (
     <section className="flex flex-col md:flex-row items-center justify-center gap-10 mt-30 p-10 sm:px-6 lg:px-8">
       <div className="w-[85vw] sm:w-5/6  md:w-[400px] md:h-[465px] lg:h-[510px] lg:w-[500px] bg-[#F8F9FD] border border-[#CECEEA] p-10 rounded-3xl overflow-hidden">
-        <h2 className="text-2xl sm:text-4xl font-bold">Bookmarks Are Broken.</h2>
+        <h2 className="text-2xl sm:text-4xl font-bold dark:text-black">Bookmarks Are Broken.</h2>
         <p className="text-[#696969] text-sm sm:text-base mt-2 relative z-99">
           Browsers, devices, tabs — they don't talk. Your bookmarks are
           scattered, hard to find, and impossible to organize.
@@ -18,7 +18,7 @@ export default function BookmarksBroken() {
           className="-mt-10"
         />
       </div>
-      <div className="w-[85vw] sm:w-5/6 md:w-[400px] md:h-[465px] lg:h-[510px] lg:w-[500px]  bg-[#000000] border border-[#CECEEA] p-10 rounded-3xl">
+      <div className="w-[85vw] sm:w-5/6 md:w-[400px] md:h-[465px] lg:h-[510px] lg:w-[500px]  bg-[#000000] border border-[#CECEEA] dark:border-gray-600 p-10 rounded-3xl">
         <div className="mb-6">
           <h2 className="text-2xl sm:text-4xl font-bold text-white">
             A Better Way to Manage Bookmarks
